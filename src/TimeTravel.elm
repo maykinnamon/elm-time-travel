a600d7d5b7fed552527e91cb17bf768f68bd791a
maykinnamon <kinnamonmay@gmail.com>
2024-11-06 21:08:00 -0600

    finished part 2 step 2

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -4,6 +4,11 @@ import Playground exposing (..)
 import Set
 
 controlBarHeight = 64
+maxVisibleHistory = 2000
+
+-- Converts an index in the history list to an x coordinate on the screen
+historyIndexToX computer index =
+  (toFloat index) / maxVisibleHistory * computer.screen.width
 
 addTimeTravel rawGame =
   { initialState = initialStateWithTimeTravel rawGame
@@ -13,10 +18,21 @@ addTimeTravel rawGame =
 
 initialStateWithTimeTravel rawGame =
   {rawModel = rawGame.initialState 
-  , paused = False}
+  , paused = False
+  , history = []}
 
 viewWithTimeTravel rawGame computer model =
   let
+    -- Creates a rectangle at the top of the screen, stretching from the
+    -- left edge up to a specific position within the history timeline
+    historyBar color opacity index =
+      let
+        width = historyIndexToX computer index
+      in
+        rectangle color width controlBarHeight  
+          |> move (computer.screen.left + width / 2)
+                  (computer.screen.top - controlBarHeight / 2)
+          |> fade opacity
     helpMessage =
         if model.paused then
           "Press R to resume"
@@ -24,8 +40,10 @@ viewWithTimeTravel rawGame computer model =
           "Press T to time travel"
   in
     (rawGame.view computer model.rawModel) ++
-      [ words white helpMessage
-          |> move 0 (computer.screen.top - controlBarHeight / 2)
+      [ historyBar black 0.3 maxVisibleHistory
+      , historyBar red 0.6 (List.length model.history) -- idk why this won't work?
+      , words white helpMessage
+        |> move 0 (computer.screen.top - controlBarHeight / 2)
       ]
 
 
@@ -37,7 +55,8 @@ updateWithTimeTravel rawGame computer model =
   else if model.paused then
     model
   else 
-    { model | rawModel = rawGame.updateState computer model.rawModel }
+    { model | rawModel = rawGame.updateState computer model.rawModel
+    , history = model.history ++ [computer] }
 
 
 keyPressed keyName computer =
